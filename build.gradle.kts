7264279aada09231be20d505a32900450ada6109
huntj88 <huntj88@gmail.com>
2020-08-22 20:18:25 -0500

    getting jitpack to work?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,13 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val ossrhUsername: String? by ext
-val ossrhPassword: String? by ext
 
 description = "A library for interacting with blocking JDBC drivers using Kotlin Coroutines."
 
 plugins {
-    `maven-publish`
-    signing
+    maven
     kotlin("jvm") version "1.4.0"
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("org.jetbrains.dokka") version "0.10.1"
-    id("net.researchgate.release") version "2.8.1"
 }
 
 repositories {
-    mavenCentral()
     jcenter()
 }
 
@@ -27,14 +17,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.9")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
     testImplementation("io.mockk:mockk:1.10.0")
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        listOf("alpha", "beta", "rc", "cr", "m", "eap", "pr").any {
-            candidate.version.contains(it, ignoreCase = true)
-        }
-    }
 }
 
 tasks.withType<KotlinCompile> {
@@ -47,95 +29,4 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     failFast = true
     useJUnitPlatform()
-}
-
-val dokka by tasks.existing(DokkaTask::class) {
-    outputFormat = "javadoc"
-    outputDirectory = "$buildDir/docs/javadoc"
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    group = LifecycleBasePlugin.BUILD_GROUP
-    description = "Assembles a jar archive containing the Javadoc API documentation."
-    archiveClassifier.set("javadoc")
-    dependsOn(dokka)
-    from(dokka.get().outputDirectory)
-}
-
-val sourcesJar by tasks.registering(Jar::class) {
-    group = LifecycleBasePlugin.BUILD_GROUP
-    description = "Assembles a jar archive containing the main classes with sources."
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
-
-publishing {
-    repositories {
-        maven {
-            if (project.version.toString().endsWith("SNAPSHOT")) {
-                setUrl("https://oss.sonatype.org/content/repositories/snapshots")
-            } else {
-                setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-            }
-
-            credentials {
-                username = ossrhUsername
-                password = ossrhPassword
-            }
-        }
-    }
-
-    publications {
-        register("mavenJava", MavenPublication::class) {
-            from(components["java"])
-            artifact(javadocJar.get())
-            artifact(sourcesJar.get())
-
-            pom {
-                name.set(project.name)
-                description.set(project.description)
-                url.set("https://github.com/michaelbull/kotlin-coroutines-jdbc")
-                inceptionYear.set("2019")
-
-                licenses {
-                    license {
-                        name.set("ISC License")
-                        url.set("https://opensource.org/licenses/isc-license.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        name.set("Michael Bull")
-                        url.set("https://www.michael-bull.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:https://github.com/michaelbull/kotlin-coroutines-jdbc")
-                    developerConnection.set("scm:git:git@github.com:michaelbull/kotlin-coroutines-jdbc.git")
-                    url.set("https://github.com/michaelbull/kotlin-coroutines-jdbc")
-                }
-
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("https://github.com/michaelbull/kotlin-coroutines-jdbc/issues")
-                }
-
-                ciManagement {
-                    system.set("GitHub")
-                    url.set("https://github.com/michaelbull/kotlin-coroutines-jdbc/actions?query=workflow%3Aci")
-                }
-            }
-        }
-    }
-}
-
-signing {
-    useGpgCmd()
-    sign(publishing.publications)
-}
-
-tasks.afterReleaseBuild {
-    dependsOn(tasks.publish)
 }
